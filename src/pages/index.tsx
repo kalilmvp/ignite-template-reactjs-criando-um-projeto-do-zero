2cec2be02a6aca1ea9b5ced6d6ab253c2a14decb
Kalil Peixoto <kalil.peixoto@jupiter.co.ao>
2021-05-08 19:55:43 +0100

    Making some changes - Full Project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -44,19 +44,6 @@ export default function Home({ postsPagination }: HomeProps) {
         ); 
       });
 
-      console.log('primeiro', postsPagination.results);
-      console.log('segundo', data.results);
-      
-      // const array1 = ['1','2','3','4'].concat(['5', '6']);
-      // console.log('array', array1);
-      
-      // const teste = Object.assign(data, {
-      //   results: [ ...postsPagination.results ].push(data.results)
-      // });
-      //
-      // console.log(teste);
-      
-      // const finalResult = [...postsPagination.results, data.results];
       setPostsPaginationState(Object.assign(data, {
         results: postsPagination.results.concat(data.results)
       }));
